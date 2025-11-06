cc81977c57b0503b807320655b4fc1d77a02e609
Maryam Abdulhakim <maryamabdulhakim2010@gmail.com>
2025-11-06 11:25:26 +0300

    project46

---
+++ project 46.py
@@ -0,0 +1,22 @@
+f1 = open("firstfile.txt", "r")
+f2 = open("secondfile.txt", "r")
+
+print("Content of the first file before appending:\n",f1.read())
+print("Content of the second file before appending:\n", f2.read())
+
+
+f1.close()
+f2.close()
+
+
+f1 = open("firstfile.txt", "a+")
+f2 = open("secondfile.txt", "r")
+
+f1.write(f2.read())
+
+
+f1.seek(0)
+
+print("Content of first file after appending:\n", f1.read())
+f1.close()
+f2.close()
